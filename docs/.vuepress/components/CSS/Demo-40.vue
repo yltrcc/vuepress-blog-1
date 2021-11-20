99bcb89f31d73bcfab3a632aad3a96fc34955c78
lastknighcoder <2223106858@qq.com>
2021-11-20 08:36:11 +0800

    ~~~

---
+++ docs/.vuepress/components/CSS/Demo-40.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="box">Hello World!</div>
+    <div class="box"><p>Welcome!</p></div>
+    <div class="box"> </div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.box {
+  width: 150px;
+  height: 50px;
+  border: 1px dashed black;
+}
+div:empty:not(:nth-child(3)) {
+  background-color: green;
+}
+</style>
---
+++ docs/.vuepress/components/CSS/Demo-41.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="class1"><p><span>Hello World!</span></p></div>
+    <div class="class2"><p><span>Hello World!</span></p></div>
+    <div class="class3"><p><span>Hello World!</span></p></div>
+    <div class="class4"><p><span>Hello World!</span></p></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* 相当于 1 个类选择其和 2 个标签选择器 */
+:is(.class1, .class3, .class4) p span{
+  color: red;
+}
+/* 相当于 2 个标签选择器 */
+:where(.class1, .class3, .class4) p span {
+  color: green;
+}
+</style>
